9865af2b20b7a52be2eaad6fa99041bc43a41a6c
LoneStar <ernieurzua@gmail.com>
2020-06-22 18:22:53 -0700

    added questions array

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var startEl = document.getElementById("start");
 var secondsLeft = 30;
+var questions = [
+    { q: "What is a data structure?", a: "a particular way of organizing data in a computer so that it can be used effectively." },
+    { q: "Which sorting method is slowest?", a: "Bogosort" },
+    { q: "What is a Binary Tree?", a: "a tree data structure in which each node has at most two children, which are referred to as the left child and the right child." },
+    { q: "Which programming language is considered a script language", a: "javascript" },
+    { q: "Which programming language is older?", a: "C" },
+    { q: "What does \"CS\"stand for ?", a: "Computer Science" },
+    { q: "What is a class?", a: "A class is a blueprint from which objects are created. A class contains methods and variables associated with an instance of a class." }]
 
 
 //function that starts the game
