bf7ca67b97aa1a74be223122d4e39c358e73a396
LoneStar <ernieurzua@gmail.com>
2020-06-23 20:44:25 -0700

    updated game logic and made css rules for some elements

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var startEl = document.getElementById("start");
-var answer01El = document.getElementById("button1");
-var answer02El = document.getElementById("button2");
-var answer03El = document.getElementById("button3");
-var answer04El = document.getElementById("button4");
+var button1El = document.getElementById("button1");
+var button2El = document.getElementById("button2");
+var button3El = document.getElementById("button3");
+var button4El = document.getElementById("button4");
+var scoreEl = document.getElementById("score")
 var selection;
 var score = 0;
 var question01El = document.querySelector(".question1");
 var secondsLeft = 5;
+var currentIndex = 0;
 var questions = [
     { q: "A data structure is a particular way of __________ data in a computer.", a: "organizing", answers: ["organizing", "cleaning", "stripping", "losing"] },
     { q: "Which sorting method is slowest?", a: "Bogosort", answers: ["bogosort", "quicksort", "mergesort", "bubblesort"] },
@@ -45,33 +47,58 @@ function start() {
     setTime(); //set timer
 
     //iterate through the questions
-    for (var i = 0; i < questions.length; i++) {
+    
+        // //display questions here
+        // question01El.textContent = questions[currentIndex].q;
 
-        //display questions here
-        question01El.textContent = questions[i].q;
-
-
-        //display answers in a button
-        answer01El.innerText = questions[i].answers[0];
-        answer02El.innerText = questions[i].answers[1];
-        answer03El.innerText = questions[i].answers[2];
-        answer04El.innerText = questions[i].answers[3];
-
-
-
-    };
+        // //display answers in a button
+        // button1El.value = questions[currentIndex].answers[0];
+        // button2El.value = questions[currentIndex].answers[1];
+        // button3El.value = questions[currentIndex].answers[2];
+        // button4El.value = questions[currentIndex].answers[3];
+        next();
 }
-console.log(questions[0].answers[0]);
-function compareAnswers() {
-    if (questions[i].a === selection) {
+
+//console.log(questions[0].answers[0]);
+
+
+function compareAnswers(selection) {
+    console.log(selection);
+    if (questions[currentIndex].a === selection) {
         score += 5;
     }
     else {
         score -= 2;
     }
 
+    currentIndex++;
+    console.log("incremented index " + currentIndex + "-question length" + questions.length);
 }
 
+function next() {
+    console.log("current Index" + currentIndex);
+   
+    if (currentIndex < questions.length ){
+     
+    //display questions here
+    question01El.textContent = questions[currentIndex].q;
+
+    //display answers in a button
+
+    button1El.value = questions[currentIndex].answers[0];
+    button2El.value = questions[currentIndex].answers[1];
+    button3El.value = questions[currentIndex].answers[2];
+    button4El.value = questions[currentIndex].answers[3];
+
+    button1El.innerText = questions[currentIndex].answers[0];
+    button2El.innerText = questions[currentIndex].answers[1];
+    button3El.innerText = questions[currentIndex].answers[2];
+    button4El.innerText = questions[currentIndex].answers[3];
+    }
+    else {
+        //end game
+    }
+}
 
 //function that clears message and displays a picture onscreen
 function sendMessage() {
@@ -86,5 +113,24 @@ function sendMessage() {
 
 //event listener for start
 startEl.addEventListener("click", start);
-//selection.addEventListener("button", compareAnswers);
+
+button1El.addEventListener("click", function() {
+    compareAnswers(this.value);//checks if the input is correct
+    next(); //loads new questions
+  });
+
+  button2El.addEventListener("click", function() {
+    compareAnswers(this.value);//checks if the input is correct
+    next(); //loads new questions
+  });
+
+  button3El.addEventListener("click", function() {
+    compareAnswers(this.value);//checks if the input is correct
+    next(); //loads new questions
+  });
+
+  button4El.addEventListener("click", function() {
+    compareAnswers(this.value);//checks if the input is correct
+    next(); //loads new questions
+  });
 
